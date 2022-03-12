83010f0fc3cfb78d607ca3b968f8b31a0903486b
Rajib0191 <rajibislam700@gmail.com>
2022-03-12 12:49:26 +0600

    first commit

---
+++ pages/checkout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const checkout = () => {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+            <h1>This Page Is Under..............</h1>
+        </div>
+    );
+};
+
+export default checkout;
